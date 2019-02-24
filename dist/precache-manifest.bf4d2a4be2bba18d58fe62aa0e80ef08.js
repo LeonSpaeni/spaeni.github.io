b7ba46e887103d72ed73dc1c81a91404a723c6c8
leonspaeni <leon.spaeni@bbzsogr.ch>
2019-02-24 09:10:58 +0100

    dist

---
+++ dist/precache-manifest.bf4d2a4be2bba18d58fe62aa0e80ef08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f05e1d44cf892aef565b",
+    "url": "/js/chunk-vendors.eca89cb8.js"
+  },
+  {
+    "revision": "3a96a5ad615ffe241027",
+    "url": "/js/app.a47586ff.js"
+  },
+  {
+    "revision": "a00efe724b7509c141d7",
+    "url": "/js/about.f04ecef3.js"
+  },
+  {
+    "revision": "701d921ca2c4c98c969ee9237dbc5e41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6038b76c953564701aa691628073bd7",
+    "url": "/img/background.f6038b76.png"
+  },
+  {
+    "revision": "f05e1d44cf892aef565b",
+    "url": "/css/chunk-vendors.f75fc4dd.css"
+  },
+  {
+    "revision": "3a96a5ad615ffe241027",
+    "url": "/css/app.17c347f9.css"
+  }
+];
